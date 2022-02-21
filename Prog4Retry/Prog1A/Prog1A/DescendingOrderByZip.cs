4ed777a75c74c6d203abc2f8a94c7d67db1464fa
Mike Berg <68578047+Mike-Berg-UL@users.noreply.github.com>
2022-02-21 17:46:28 -0500

    Add files via upload

---
+++ Prog4Retry/Prog1A/Prog1A/DescendingOrderByZip.cs
@@ -0,0 +1,40 @@
+﻿// Program 4
+// CIS 200-01
+// Fall 2021
+// Due: 12/2/2021
+// By: Michael Bergamini 1416810
+
+// File: DescendingOrderByZip.cs
+// sorts the list of parcels in descending order by zip
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prog1
+{
+    class DescendingOrderByZip : Comparer<Parcel>
+    {
+        //precondition: none
+        //postcondition: compares parcels if not null
+        public override int Compare(Parcel x, Parcel y)
+        {
+            if (x == null && y == null)
+                return 0;
+
+            if (x == null)
+                return -1;
+
+            if (y == null)
+                return 1;
+
+            int xZip = x.DestinationAddress.Zip; //store zip 
+            int yZip = y.DestinationAddress.Zip; //stores zip
+            
+            return (-1) * xZip.CompareTo(yZip); // Reverses natural order, so descending
+        }
+    }
+}
