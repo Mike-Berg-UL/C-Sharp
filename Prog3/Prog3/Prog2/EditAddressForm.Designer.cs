9c04a490e8235dfc79c0fd85d2d5460742085b10
Mike Berg <68578047+Mike-Berg-UL@users.noreply.github.com>
2022-02-21 17:42:58 -0500

    Add files via upload

---
+++ Prog3/Prog3/Prog2/EditAddressForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace UPVApp
+{
+    partial class EditAddress
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.editAddressCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.OkEditAddress = new System.Windows.Forms.Button();
+            this.CancelEditAddress = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // editAddressCombo
+            // 
+            this.editAddressCombo.FormattingEnabled = true;
+            this.editAddressCombo.Location = new System.Drawing.Point(159, 94);
+            this.editAddressCombo.Name = "editAddressCombo";
+            this.editAddressCombo.Size = new System.Drawing.Size(121, 24);
+            this.editAddressCombo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(140, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select an Address to Edit";
+            // 
+            // OkEditAddress
+            // 
+            this.OkEditAddress.Location = new System.Drawing.Point(71, 187);
+            this.OkEditAddress.Name = "OkEditAddress";
+            this.OkEditAddress.Size = new System.Drawing.Size(124, 44);
+            this.OkEditAddress.TabIndex = 2;
+            this.OkEditAddress.Text = "Edit";
+            this.OkEditAddress.UseVisualStyleBackColor = true;
+            this.OkEditAddress.Click += new System.EventHandler(this.OkEditAddress_Click);
+            // 
+            // CancelEditAddress
+            // 
+            this.CancelEditAddress.Location = new System.Drawing.Point(252, 187);
+            this.CancelEditAddress.Name = "CancelEditAddress";
+            this.CancelEditAddress.Size = new System.Drawing.Size(115, 44);
+            this.CancelEditAddress.TabIndex = 3;
+            this.CancelEditAddress.Text = "Cancel";
+            this.CancelEditAddress.UseVisualStyleBackColor = true;
+            this.CancelEditAddress.Click += new System.EventHandler(this.CancelEditAddress_Click);
+            // 
+            // EditAddress
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 255);
+            this.Controls.Add(this.CancelEditAddress);
+            this.Controls.Add(this.OkEditAddress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.editAddressCombo);
+            this.Name = "EditAddress";
+            this.Text = "Edit Address";
+            this.Load += new System.EventHandler(this.EditAddress_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox editAddressCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button OkEditAddress;
+        private System.Windows.Forms.Button CancelEditAddress;
+    }
+}
---
+++ Prog3/Prog3/Prog2/EditAddressForm.cs
@@ -0,0 +1,77 @@
+﻿// Program 3
+// CIS 200
+// Fall 2021
+// Due: 11/15/2021
+// By: 1416810
+// File: Prog3Form.cs
+// This class lets the user choose an address object to edit and
+// returns the selected index in the List of addresses
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UPVApp
+{
+    [Serializable] //lets data be serialized        
+    
+    public partial class EditAddress : Form
+    {
+
+        private List<Address> addressList;//list of addresses
+        public EditAddress(List<Address> addresses)//loads list of addresses
+        {
+            InitializeComponent();//opens window
+            addressList = addresses;
+        }
+
+        public int AddressIndex
+        {
+            // Precondition:  User has selected from destAddCbo
+            // Postcondition: The index of the selected origin address returned
+            get
+            {
+                return editAddressCombo.SelectedIndex;
+            }
+
+            // Precondition:  -1 <= value < addressList.Count
+            // Postcondition: The specified index is selected in destAddCbo
+            set
+            {
+                if ((value >= -1) && (value < addressList.Count))
+                    editAddressCombo.SelectedIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException("DestinationAddressIndex", value,
+                        "Index must be valid");
+            }
+        }
+        //Procondition: Cancel Button Clicked
+        //Postcondition: window closed
+        private void CancelEditAddress_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }               
+       
+        //Precondition: Ok button clicked
+        //Postcondition: THIS selected index is sent to the edit address form
+        private void OkEditAddress_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+        //Precondtion: edit address form is loaded
+        //Postcondition: Name properties of address objects loaded into combobox
+        private void EditAddress_Load(object sender, EventArgs e)
+        {
+            foreach (Address a in addressList)
+            {
+                editAddressCombo.Items.Add(a.Name);
+            }
+        }
+    }
+    
+}
