dbe194d7ed4ea71de003cfbb34ceb3d252d6e9be
Mike Berg <68578047+Mike-Berg-UL@users.noreply.github.com>
2022-02-21 21:36:55 -0500

    Add files via upload

---
+++ Prog1A/Prog0/AirPackage.cs
@@ -0,0 +1,58 @@
+﻿//Student ID: 1416810
+// Program 1A
+// CIS 200-50
+// Fall 2020
+// Due: 9/27/2021
+//File: AirPackage.cs
+// This classes inherits from Package and creates an air package object
+// two addresses, length, width, heigt, weight
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public abstract class AirPackage : Package
+{
+    //Precondition: none
+    //Postcondition: AirPackage created with specified values for addresses, length, width, height, weight
+    public AirPackage(Address originAddress, Address destAddress, double length, double width,
+        double height, double weight)
+        : base(originAddress, destAddress, length, width, height, weight) 
+    { }
+
+    //Precondition: none
+    //Postcondition: method returns boolian value if packge weight > 75
+    public bool IsHeavy()
+    {
+        bool isHeavy = false;
+
+        if (Weight > 75)
+        {
+            isHeavy = true;
+        }
+        return isHeavy;
+    }
+    //Precondition: none
+    //Postcondition: method returns boolian value if length + width + height > 75
+    public bool IsLarge()
+    {
+        bool isLarge = false;
+
+        if (Length + Width + Height > 100)
+        {
+            isLarge = true;
+        }
+        return isLarge;
+    }
+    //Precondition: none
+    //Postcondition: a string with AirPackages data is returned
+    public override string ToString()
+    {
+        string NL = Environment.NewLine; // NewLine shortcut
+
+        return $"{base.ToString()} {NL}Heavy Item: {IsHeavy()} {NL}Large Item: {IsLarge()}";
+    }
+
+}
+
---
+++ Prog1A/Prog0/GroundPackage.cs
@@ -0,0 +1,50 @@
+﻿//Student ID: 1416810
+// Program 1A
+// CIS 200-50
+// Fall 2020
+// Due: 9/27/2021
+//File: GroundPackage.cs
+// This class inherits from Package and Creates a Ground Package
+// two addresses, length, width, height weight
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+class GroundPackage : Package
+{
+    const int FIRST_DIGIT_FACTOR = 10000; //used to find first digit value of Zip code
+
+    //Precondition: none
+    //Postcondition: GroundPackade is created with specified values for addresses, length, width, height, weight
+    public GroundPackage(Address originAddress, Address destAddress, double length,
+        double width, double height, double weight)
+        : base(originAddress, destAddress, length, width, height, weight)
+    {
+        ZoneDistance = Math.Abs((originAddress.Zip / FIRST_DIGIT_FACTOR) - (destAddress.Zip / FIRST_DIGIT_FACTOR)); //Zip code equation
+    }
+
+    
+    public int ZoneDistance //read only helper property
+    { get; }
+        
+    //Procondition: none
+    //PostCondition: GroundPackage cost is returned
+    public override decimal CalcCost()
+    {
+        double Cost = .15 * (Length + Width + Height) + .07 * (ZoneDistance + 1) * (Weight);
+
+        return (decimal)Cost;
+    }
+
+    //Precondition: none
+    //Postcondition: a string with GroundPackage's data is returned
+    public override string ToString()
+    {
+        string NL = Environment.NewLine; // NewLine shortcut
+
+        return  $"{base.ToString()} {NL}ZoneDistance: {ZoneDistance}";
+    }
+}
+
